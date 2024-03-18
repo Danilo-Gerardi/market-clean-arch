7ec77fa9288ef6425d037d01f5cb8cfe3e5a2b89
Danilo Gerardi de Faria <danilogerardi@gmail.com>
2024-03-17 22:36:05 -0300

    tela de compra - css concluído

--- src/app/infra/ui/layouts/main-layout/pages/purchase/purchase.module.ts
+++ src/app/infra/ui/layouts/main-layout/pages/purchase/purchase.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FormatToRealPipe } from 'src/app/infra/pipes/format-to-real.pipe';
 import { SharedModule } from 'src/app/infra/ui/shared/shared/shared.module';
 import { PurchaseRoutingModule } from './purchase-routing.module';
 import { PurchaseComponent } from './purchase.component';
 
 @NgModule({
-  declarations: [PurchaseComponent],
+  declarations: [PurchaseComponent, FormatToRealPipe],
   imports: [CommonModule, PurchaseRoutingModule, SharedModule],
 })
 export class PurchaseModule {}
